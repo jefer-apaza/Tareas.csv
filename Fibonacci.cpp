9deaabbb33ff9a7d001a0527d27f8157ff10ff24
jefer-apaza <jeferapazaa@gmail.com>
2022-04-04 23:43:22 -0500

    Tarea_1

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -2,13 +2,14 @@
 
 using namespace std;
 
-void fibonacci (int x) {
+int fibonacci (int x) {
+
 	if ( x == 0 ) {
 
 		return 0;
 
-	} if else ( x == 1 ) {
-		
+	} if ( x == 1 ) {
+
 		return 1;
 
 	} else {
@@ -23,14 +24,14 @@ int main () {
 	int x = 1;
 
 	while ( x != 0 ) {
-		
+
 		int n;
 
-		cout << "Introduce un valor para la serie Fibonacci: ";
+		cout << "\nIntroduce un valor para la serie Fibonacci: ";
 		cin >> n;
 		if ( n < 0 ) {
 
-			print("El valor introducido es negativo no se puede hacer una serie Fibonacci");
+			cout<<"\nEl valor introducido es negativo no se puede hacer una serie Fibonacci";
 
 			x = 1;
 
@@ -39,11 +40,11 @@ int main () {
 			cout << " \nLa seria Fibonacci es: ";
 
 			for (int i = 0; i < n; i++)
-				cout << i + 1 << ": " << fibonacci(i);
-					
+				cout << fibonacci(i)<<", ";
+
 		}
 
-		cout << "Para continuar el programa inserte un valor distinto de 0: ";
+		cout << "\n\nPara continuar el programa inserte un valor distinto de 0: ";
 		cin >> x;
 	}
 
